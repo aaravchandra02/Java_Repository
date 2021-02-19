8eab9f3bdbcfec1c806fc7bb713b3ae1ee0bc2d7
Aarav Chandra <aaravchandra02@gmail.com>
2021-02-19 15:21:51 -0800

    added getter and setter

--- SpringBoot/Learning_SpringBoot/demo01/src/main/java/com/aarav_chandra/demo01/model/Person.java
+++ SpringBoot/Learning_SpringBoot/demo01/src/main/java/com/aarav_chandra/demo01/model/Person.java
@@ -6,10 +6,20 @@ public class Person {
     private final UUID id;
     private final String name;
 
+    // Constructor
     public Person(UUID id_ini, String name_ini) {
         this.id = id_ini;
         this.name = name_ini;
-
+    }
+    // getter method
+    public UUID getUuid() {
+        return this.id;
+    }
+    // getter method
+    public String name() {
+        return this.name;
+        
+    }
     }
 
 }
