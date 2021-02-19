cbd6dcc282d1570967e7c85f9af39377cf6ef9ec
Aarav Chandra <aaravchandra02@gmail.com>
2021-02-19 15:18:09 -0800

    initial commit

---
+++ SpringBoot/Learning_SpringBoot/demo01/src/main/java/com/aarav_chandra/demo01/model/Person.java
@@ -0,0 +1,15 @@
+package com.aarav_chandra.demo01.model;
+
+import java.util.*;
+
+public class Person {
+    private final UUID id;
+    private final String name;
+
+    public Person(UUID id_ini, String name_ini) {
+        this.id = id_ini;
+        this.name = name_ini;
+
+    }
+
+}
---
+++ SpringBoot/Learning_SpringBoot/demo01/src/test/java/com/aarav_chandra/demo01/Demo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarav_chandra.demo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
